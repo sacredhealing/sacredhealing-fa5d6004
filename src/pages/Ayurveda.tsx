898ff2d622a949f33d70afb04679714ff4cf88e1
sacredhealing <sacredhealingvibe@gmail.com>
2026-06-02 19:35:15 +0200

    redeploy: remove duplicate Agastya chat [20260602-173515]

--- src/pages/Ayurveda.tsx
+++ src/pages/Ayurveda.tsx
@@ -1,4 +1,4 @@
-// cache-bust: 20260602-173023
+// cache-bust: 20260602-173515
 import React from 'react';
 import { AyurvedaTool } from '@/components/ayurveda/AyurvedaTool';
 import { useAuth } from '@/hooks/useAuth';
