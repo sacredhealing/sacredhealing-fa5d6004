0a31083dc4c0c970c1f3f2f125ccaf3f7bb4aeab
sacredhealing <sacredhealingvibe@gmail.com>
2026-06-02 18:36:07 +0200

    redeploy: force Lovable rebuild after isSiddhaPlus TS fix [20260602-163606]

--- src/pages/Ayurveda.tsx
+++ src/pages/Ayurveda.tsx
@@ -1,4 +1,4 @@
-// cache-bust: 20260602-162724
+// cache-bust: 20260602-163606
 import React from 'react';
 import { AyurvedaTool } from '@/components/ayurveda/AyurvedaTool';
 import { useAuth } from '@/hooks/useAuth';
