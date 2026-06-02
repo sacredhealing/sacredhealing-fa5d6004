2365c1e27c98b66aefdd602f5c29441991d83ca1
sacredhealing <sacredhealingvibe@gmail.com>
2026-06-02 19:30:23 +0200

    redeploy: admin full Akasha access [20260602-173023]

--- src/pages/Ayurveda.tsx
+++ src/pages/Ayurveda.tsx
@@ -1,4 +1,4 @@
-// cache-bust: 20260602-165447
+// cache-bust: 20260602-173023
 import React from 'react';
 import { AyurvedaTool } from '@/components/ayurveda/AyurvedaTool';
 import { useAuth } from '@/hooks/useAuth';
