9e9f376bd8af6a492c1a1ffba33149f8f21a442f
sacredhealing <sacredhealingvibe@gmail.com>
2026-06-05 12:45:38 +0200

    Update SSL fix script with better error handling

--- scripts/clawbot_ssl_fix.py
+++ scripts/clawbot_ssl_fix.py
@@ -1,9 +1,13 @@
-import urllib.request, json, time, os, sys
+import urllib.request, json, time, os, sys, traceback
 
+print("Script starting...", flush=True)
 token = os.environ.get('RAILWAY_TOKEN', '')
+print(f"RAILWAY_TOKEN length: {len(token)}", flush=True)
 if not token:
-    print("ERROR: RAILWAY_TOKEN not set")
+    print("ERROR: RAILWAY_TOKEN not set", flush=True)
     sys.exit(1)
+
+print(f"Token prefix: {token[:8]}...", flush=True)
 
 SERVICE_ID = "b6311e53-fe5a-4c4a-b724-e0fef686236f"
 DOMAIN = "clawbot.siddhaquantumnexus.com"
@@ -26,10 +30,14 @@ def log(msg):
 log("=== CLAWBOT Domain Refresh ===")
 
 try:
+    log("Calling Railway GraphQL API...")
     r = gql('{ service(id: "' + SERVICE_ID + '") { id name domains { edges { node { id domain status } } } } }')
     log("Domains response: " + json.dumps(r))
 except Exception as ex:
     log("GQL ERROR: " + str(ex))
+    log(traceback.format_exc())
+    with open("_clawbot_ssl_result.txt", "w") as f:
+        f.write("\n".join(LOG))
     sys.exit(1)
 
 edges = r.get("data",{}).get("service",{}).get("domains",{}).get("edges",[])
@@ -42,22 +50,32 @@ for e in edges:
 
 if target_id:
     log("Deleting stuck domain: " + target_id)
-    r2 = gql('mutation { customDomainDelete(id: "' + target_id + '") }')
-    log("Delete result: " + json.dumps(r2))
+    try:
+        r2 = gql('mutation { customDomainDelete(id: "' + target_id + '") }')
+        log("Delete result: " + json.dumps(r2))
+    except Exception as ex:
+        log("DELETE ERROR: " + str(ex))
     log("Sleeping 10s...")
     time.sleep(10)
 else:
     log("No existing clawbot domain found - will add fresh")
 
 log("Adding domain: " + DOMAIN)
-r3 = gql('mutation { customDomainCreate(input: { serviceId: "' + SERVICE_ID + '", domain: "' + DOMAIN + '" }) { id domain status } }')
-log("Add result: " + json.dumps(r3))
+try:
+    r3 = gql('mutation { customDomainCreate(input: { serviceId: "' + SERVICE_ID + '", domain: "' + DOMAIN + '" }) { id domain status } }')
+    log("Add result: " + json.dumps(r3))
+except Exception as ex:
+    log("ADD ERROR: " + str(ex))
+    log(traceback.format_exc())
 
 log("Waiting 30s for SSL provisioning...")
 time.sleep(30)
 
-r4 = gql('{ service(id: "' + SERVICE_ID + '") { domains { edges { node { id domain status } } } } }')
-log("Final domain list: " + json.dumps(r4))
+try:
+    r4 = gql('{ service(id: "' + SERVICE_ID + '") { domains { edges { node { id domain status } } } } }')
+    log("Final domain list: " + json.dumps(r4))
+except Exception as ex:
+    log("FINAL CHECK ERROR: " + str(ex))
 
 for url in ["https://siddha-soma-apothecary-production.up.railway.app/health", "https://" + DOMAIN + "/health"]:
     try:
@@ -71,3 +89,4 @@ with open("_clawbot_ssl_result.txt", "w") as f:
     f.write("\n".join(LOG))
 
 log("Script complete.")
+print("Done.", flush=True)
