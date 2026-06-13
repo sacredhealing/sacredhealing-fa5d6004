75c2d15ed218e4ad5ed0e5badb0ec681ade8d203
sacredhealing <sacredhealingvibe@gmail.com>
2026-06-13 11:41:35 +0200

    feat: add /brahmacharya-academy route and lazy import for BrahmacharyaAcademy

--- src/App.tsx
+++ src/App.tsx
@@ -206,6 +206,7 @@ const VayuProtocol = React.lazy(() => import("./pages/VayuProtocol"));
 const SiddhaPortal = React.lazy(() => import("./pages/SiddhaPortal"));
 const OjasRasayanaAcademy = React.lazy(() => import("./pages/OjasRasayanaAcademy"));
 const KayakalpaAcademy = React.lazy(() => import("./pages/KayakalpaAcademy"));
+const BrahmacharyaAcademy = React.lazy(() => import("./pages/BrahmacharyaAcademy"));
 const SiddhaDreamAcademy = React.lazy(() => import("./pages/SiddhaDreamAcademy"));
 // WomanCode merged into Shakti Cycle Intelligence
 const AdminQuantumApothecary2045 = React.lazy(() => import("./pages/AdminQuantumApothecary2045"));
@@ -447,6 +448,7 @@ function AppRoutes() {
                   <Route path="/siddha-portal" element={<SiddhaPortal />} />
                    <Route path="/ojas-rasayana" element={<OjasRasayanaAcademy />} />
                    <Route path="/kayakalpa-academy" element={<KayakalpaAcademy />} />
+                   <Route path="/brahmacharya-academy" element={<BrahmacharyaAcademy />} />
                   <Route path="/dream-academy" element={<SiddhaDreamAcademy />} />
                   <Route path="/admin-quantum-apothecary-2045" element={<AdminQuantumApothecary2045 />} />
                   <Route path="/sovereign-hormonal-alchemy" element={<SovereignHormonalAlchemy />} />
@@ -557,6 +559,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
